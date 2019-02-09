9bbf485786690093b805da3af592dfba25dfadcc
Rajat Kanti Nandi <rajatkantinandi1993@gmail.comi>
2019-02-10 03:47:26 +0530

    created coding profile

--- js/index.js
+++ js/index.js
@@ -1,27 +1,4 @@
-$(document).ready(function(){
-    $(".navbar-collapse a").click(function(){
-        $(".navbar-collapse li.active").removeClass("active");
-        $(this).parent().addClass("active");
-        $(".navbar-collapse").collapse('hide');
-    });
-  window.onscroll = function() {
-    if ( document.body.scrollTop > 10 &&document.body.scrollTop < 350|| document.documentElement.scrollTop > 10  && document.documentElement.scrollTop < 350 ) {
-        $("#e1").addClass("active");
-        $("#e2").removeClass("active");
-        $("#e3").removeClass("active");
-        $(".navbar-header img").css("display","none");
-    }
-   else if (document.body.scrollTop > 350 && document.body.scrollTop < 1700|| document.documentElement.scrollTop > 350 && document.documentElement.scrollTop < 1700 ) {
-        $("#e2").addClass("active");
-        $("#e1").removeClass("active");
-        $("#e3").removeClass("active");        
-        $(".navbar-header img").css("display","inline");
-    }
-    else if (document.body.scrollTop > 1700 || document.documentElement.scrollTop > 1700 ) {
-        $("#e3").addClass("active");
-        $("#e2").removeClass("active");
-        $("#e1").removeClass("active");
-        $(".navbar-header img").css("display","inline");
-    }
-  }
+document.querySelectorAll(".points").forEach(elem => {
+  const point = elem.getAttribute("data-point") + "%";
+  elem.querySelector(".fill").style.width = point;
 });
